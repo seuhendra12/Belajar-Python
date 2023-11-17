32ac72aef9be553f86d3aaa08780c99809c4a7cc
Seuhendra Setiawan <seuhendra12@gmail.com>
2023-11-17 19:24:26 +0700

    first commit

---
+++ hello.py
@@ -0,0 +1,26 @@
+print("Hello World")
+
+print(1 +3)
+
+print(50*'Hello')
+
+print("Hello"[2])
+
+# Split
+teks = "Hello, nama saya, budi"
+x = teks.split(",")
+print(x)
+
+# Is Lower
+a = "Hello"
+b = "hello"
+print(a.islower())
+print(b.islower())
+
+# Count
+teks = "Hallo semuanya, disini saya bersama dengan budi semuanya"
+x = teks.count("semuanya")
+print(x)
+
+teks = "halo, nama saya budi"[-1]
+print(teks)
---
+++ operator-basic.py
@@ -0,0 +1,17 @@
+# Penjumlahan
+print(100+10) 
+
+# pengurangan
+print(100-10)
+
+# perkalian
+print(10*10)
+
+# pembagian
+print(100/10)
+
+# hasil bagi
+print(100//10)
+
+# sisa bagi
+print(100%30)
