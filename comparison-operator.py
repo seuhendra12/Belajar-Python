f373af1acc4b3691ac6b43e2f11c50f907e1cfc2
Seuhendra Setiawan <seuhendra12@gmail.com>
2023-12-01 08:53:32 +0700

    Menambahkan logical-operator

--- comparison-operator.py
+++ comparison-operator.py
@@ -3,3 +3,22 @@ print('Result of 5 < 1: ', 5 < 1)
 print('Result of 5 == 5', 5 == 5)
 print('Result of 5 != 5', 5 != 5)
 print("Result of 'a' > 'b': ", 'a' > 'b')
+
+# Comparison operator is
+a = 1
+b = 1.0
+
+print (a == 1)
+print (a is b)
+
+#Comparison operator in'print('aaa' in 'aaa-bbb-ccc')
+print('aaa' in 'aaa-bbb-ccc')
+print('bbb' in 'aaa-bbb-ccc')
+print('ddd' in 'aaa-bbb-ccc')
+
+a = 'abc'
+b = 'ABC'
+c = 'abC-abc-aBc'
+
+print( a is b )
+print( a in c )
