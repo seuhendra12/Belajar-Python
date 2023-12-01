3027c206ec8d3f85db88961842c009a6bb61d56c
Seuhendra Setiawan <seuhendra12@gmail.com>
2023-12-01 09:07:26 +0700

    Menambahkan if-conftion

---
+++ if-condition.py
@@ -0,0 +1,32 @@
+age = 18
+if age < 20:
+    print('Yes he is 18 years old')
+print('Welcome')
+
+age = 24
+if age < 20:
+    print('Yes he is 18 years old')
+print('Welcome')
+
+# PASS
+num = 2
+if num % 2 == 0:
+    print('Nomor genap')
+if num % 3 == 0:
+    pass
+
+# LATIHAN
+a = "ABC"
+b = "abc"
+c = "ABC"
+d = "CBA"
+
+if a == b:
+    print("123")
+elif a == c:
+    print("456")
+
+if a == b:
+    print("1234")
+else:
+    print("789")
